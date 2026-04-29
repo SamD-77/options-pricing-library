bc49a1592a53a49788d284c5ee3cce9adb114177
SamD-77 <223775524+SamD-77@users.noreply.github.com>
2026-04-29 12:19:43 -0400

    Add initial Black-Scholes class with call and put pricing

---
+++ black-scholes.py
@@ -0,0 +1,37 @@
+import math
+from scipy.stats import norm
+
+class BlackScholes:
+    def __init__(self, S, K, r, t, sigma):
+        """
+        Black-Scholes model for European option pricing.
+
+        Parameters
+        ----------
+        S : float
+            Current underlying price
+        K : float
+            Strike price
+        r : float
+            Risk-free interest rate
+        t : float
+            Time to maturity
+        sigma : float
+            Annualized volatility
+        """
+        self.S = S
+        self.K = K
+        self.r = r
+        self.t = t
+        self.sigma = sigma
+
+        self.d1 = (math.log(S / K ) + (r + (sigma**2 / 2)) * t) / (sigma * math.sqrt(t))
+        self.d2 = self.d1 - sigma * math.sqrt(t)
+
+    def call_price(self):
+        """Price of a European call option"""
+        return self.S * norm.cdf(self.d1) - self.K * math.exp(-self.r * self.t) * norm.cdf(self.d2)
+    
+    def put_price(self):
+        """Price of a European put option"""
+        return self.K * math.exp(-self.r * self.t) * norm.cdf(-self.d2) - self.S * norm.cdf(-self.d1)
